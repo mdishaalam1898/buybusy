60908de8e8d1bc3b6e6af62bba9836bd2ab4c723
mdishaalam1898 <mdishaalam560@gmail.com>
2023-12-10 15:37:09 +0530

    first

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,74 +10,75 @@ export default function Navbar() {
 
   return (
     <>
-       {/* main container */}
-       <div className={styles.navbar}> 
-                {/* app heading */}
-                <div className={styles.appName}>
-                    <NavLink to="/">
-                        {/* logo of the app */}
-                        <i class="fa-solid fa-shop"></i>
-                        Buy Busy
-                    </NavLink>
-                </div>
+      {/* main container */}
+      <div className={styles.navbar}>
+        {/* app heading */}
+        <div className={styles.appName}>
+          <NavLink to="/">
+            {/* logo of the app */}
+            <i class="fa-solid fa-shop"></i>
+            Buy Busy
+          </NavLink>
+        </div>
+        {/* all the navigation link */}
+        <div className={styles.navLinks}>
+          {/* homepage link */}
+          <NavLink to="/">
+            <span>
+              {/* home logo */}
+              <i class="fa-solid fa-house"></i>
+              Home
+            </span>
+          </NavLink>
 
-                {/* all the navigation link */}
-                <div className={styles.navLinks}>
+          {/* myorder link */}
+          {/* show when user is logged in */}
+          {isLoggedIn && (
+            <NavLink to="/myorder">
+              <span>
+                {/* my order logo */}
+                <i class="fa-solid fa-bag-shopping"></i>
+                My Order
+              </span>
+            </NavLink>
+          )}
 
-                    {/* homepage link */}
-                    <NavLink to="/">
-                        <span>
-                            {/* home logo */}
-                            <i class="fa-solid fa-house"></i>
-                            Home
-                        </span>
-                    </NavLink>
+          {/* cart link */}
+          {/* show when user is logged in */}
+          {isLoggedIn && (
+            <NavLink to="/cart">
+              <span>
+                {/* cart icon */}
+                <i class="fa-sharp fa-solid fa-cart-shopping"></i>
+                Cart
+              </span>
+            </NavLink>
+          )}
 
-                    {/* myorder link */}
-                    {/* show when user is logged in */}
-                    {isLoggedIn && <NavLink to="/myorder">
-                        <span>
-                            {/* my order logo */}
-                            <i class="fa-solid fa-bag-shopping"></i>
-                            My Order
-                        </span>
-                    </NavLink> }
+          {/* for signIN and signOut */}
+          <NavLink to={!isLoggedIn ? "/signin" : "/"}>
+            <span>
+              {!isLoggedIn ? (
+                <>
+                  {/* sign in icon */}
+                  <i class="fa-solid fa-right-to-bracket"></i>
+                  SignIn
+                </>
+              ) : (
+                <>
+                  {/* sign out icon */}
+                  <i class="fa-solid fa-right-from-bracket"></i>
+                  {/* sign out user  */}
+                  <span onClick={SignOut}>SignOut</span>
+                </>
+              )}
+            </span>
+          </NavLink>
+        </div>
+      </div>
 
-
-                    {/* cart link */}
-                    {/* show when user is logged in */}
-                    {isLoggedIn && <NavLink to="/cart">
-                        <span>
-                            {/* cart icon */}
-                            <i class="fa-sharp fa-solid fa-cart-shopping"></i>
-                            Cart
-                        </span>
-                    </NavLink> }
-
-
-                    {/* for signIN and signOut */}
-                    <NavLink to={!isLoggedIn?"/signin":"/"}>
-                        <span>
-                            {!isLoggedIn?
-                                <>
-                                    {/* sign in icon */}
-                                    <i class="fa-solid fa-right-to-bracket"></i>
-                                    SignIn
-                                </>
-                                :
-                                <>
-                                    {/* sign out icon */}
-                                    <i class="fa-solid fa-right-from-bracket"></i>
-                                    {/* sign out user  */}
-                                    <span onClick={SignOut}>SignOut</span>
-                                </>
-                            }
-                        </span>
-                    </NavLink>
-                </div>
-            </div>
-            {/* render child pages */}
-            <Outlet />
+      {/* render child pages */}
+      <Outlet />
     </>
   );
 }
